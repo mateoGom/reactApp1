e27dfefe87198b9d0eadb5615653886f13ee46b0
mateoGom <matemgm@hotmail.com>
2020-10-17 15:55:36 +0200

    props

---
+++ src/Person/Person.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const person = (props)=>{
+    return(
+        <div>
+            <p>I'm {props.name} and i am {props.age} yo</p>
+            <p>{props.children}</p>
+
+        </div>
+    )
+}
+
+export default person;
