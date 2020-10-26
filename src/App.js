cf185e4d570a676b0f6dbdc46da1ed32cb80bdf0
mateoGom <matemgm@hotmail.com>
2020-10-26 15:10:52 +0100

    setting styles dynamically

--- src/App.js
+++ src/App.js
@@ -59,7 +59,7 @@ class App extends Component {
 
   render() {
     const style = {
-      backgroundColor: "white",
+      backgroundColor: "green",
       font: "inherit",
       border: "1px solid blue",
       padding: "8px",
@@ -83,6 +83,7 @@ class App extends Component {
           })}
         </div>
       );
+      style.backgroundColor='red';
     }
 
     return (
