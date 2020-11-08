c8d149f282f74e758fe81b19370696f46cefb2ea
mateoGom <matemgm@hotmail.com>
2020-11-09 00:20:23 +0100

    introduction to styled components

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,8 +1,22 @@
 import React from "react";
-import Radium from 'radium';
+import Styled from 'styled-components';
+//import "./Person.css";
 
-import "./Person.css";
+const StyledDiv = Styled.div`
 
+  width: 60%;
+  margin: 16px auto;
+  border: 1px solid #eee;
+  box-shadow: 0 2px 3px #ccc;
+  padding: 16px;
+  text-align: center;
+
+
+@media (min-width: 500px):{
+  width: 450px;
+}
+
+`;
 const person = (props) => {
 
 const style = {
@@ -12,14 +26,14 @@ const style = {
 }
 
   return (
-    <div className="Person" style={style}>
+    <StyledDiv>
       <p onClick={props.click}>
         I'm {props.name} and i am {props.age} yo
       </p>
 
       <input type="text" onChange={props.changed} value={props.name} />
-    </div>
+      </StyledDiv>
   );
 };
 
-export default Radium(person);
+export default person;
